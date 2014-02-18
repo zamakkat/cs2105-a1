1dddf961e7c770adbf2d9f36c53db0e9fd67b452
Duong Ho Tuan <zamakkat@gmail.com>
2014-02-18 12:35:03 +0800

    newest running version

--- WebServer.java
+++ WebServer.java
@@ -15,7 +15,49 @@ class WebServer {
 	// stored.  You need to change this to your own local
 	// directory if you want to play with this server code.
 	static String WEB_ROOT = "/Users/zamakkat1/Dropbox/Study/Sem6/CS2105/Assignments/cs2105-a1";
-
+	
+	private static String runPerlScript(String scriptPath, String[] env) throws IOException {
+		System.out.println("Executing Perl script: " + scriptPath);
+		// Execute script with Environment variables given in String[] env
+		Process todo = Runtime.getRuntime().exec("/usr/bin/perl " + scriptPath, env);
+		
+		// Get process output
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(todo.getInputStream()));
+	    //BUG HERE WITH POST!!!!
+	    String l = reader.readLine();
+	    String output = "";
+	    while (l!=null) {
+	    	output += l + "\r\n";
+	    	l = reader.readLine();
+	    }
+	    System.out.println(output);
+		return output;
+	}
+	
+	private static String error404(String item) {
+		String output = "";
+		String errorMessage = "I cannot find " + item + " on this server.\r\n";
+		output += "HTTP/1.1 404 Not Found\r\n";
+		output += "Content-length: " + errorMessage.length() + "\r\n\r\n";
+		return output;
+	}
+	
+	private static String error403(String item) {
+		String output = "";
+		String errorMessage = "You have no permission to access " + item + " on this server.\r\n";
+		output += "HTTP/1.1 403 Forbidden\r\n";
+		output += "Content-length: " + errorMessage.length() + "\r\n\r\n";
+		return output;
+	}
+	
+	private static String error400() {
+		String output = "";
+		String errorMessage = "400 Bad Request\r\n";
+		output += "HTTP/1.1 400 Bad Request\r\n";
+		output += "Content-length: " + errorMessage.length() + "\r\n\r\n";
+		return output;
+	}
+	
 	public static void main(String args[]) 
 	{
 		ServerSocket serverSocket;
@@ -78,102 +120,72 @@ class WebServer {
 				// We only care about the first two tokens here.
 				String tokens[] = line.split(" ");
 				
-				String filepath = "";
-				String filename = "";
+				String scriptPath = "";
+				String scriptName = "";
 				String req_method = "";
 				String query_string = "";
 				String content_type = "";
 				String content_length = "";
 				
-				if (tokens[0].equals("GET")) {
-					//TODO: Put the get request code here
-					
+				if (tokens[0].equals("GET")) {					
 					// We do not really care about the rest of the HTTP
 					// request header either.  Read them off the input
 					// and throw them away.
-					while (!line.equals("")) 
-					{
+					while (!line.equals("")) {
 						line = br.readLine();
 					}
 
-					// Print to screen so that we have a log of client's 
-					// requests.
-					System.out.println("GET " + tokens[1]);
-					
-					// The second token indicates the filename.
-					filepath = WEB_ROOT + tokens[1];
-					filename = tokens[1];
-					
-					req_method = "GET";
-					query_string = "";
-					
-				} else if (tokens[0].equals("POST")) {
-					
-					
-					
-					
-					
-					
-					req_method = "POST";
-					content_type = "";
-					content_length = "";
-					
-				} else {
-					String errorMessage = "This simplistic server only understand GET request\r\n";
-					dos.writeBytes("HTTP/1.1 400 Bad Request\r\n");
-					dos.writeBytes("Content-length: " + errorMessage.length() + "\r\n\r\n");
-					dos.writeBytes(errorMessage);
-					s.close();
-					continue;
-				}
+					int argIndex = tokens[1].indexOf('?');
+					if (argIndex == -1) {
+						scriptName = tokens[1];
+						query_string = "";
+					} else {
+						scriptName = tokens[1].substring(0, argIndex);
+						query_string = tokens[1].substring(argIndex+1);
+					}
+					scriptPath = WEB_ROOT + scriptName;
 
-				File file = new File(filepath);
-				
-				// Check for file permission or not found error.
-				if (!file.exists() || !file.isFile()) {
-					String errorMessage = "I cannot find " + filename + " on this server.\r\n";
-					dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
-					dos.writeBytes("Content-length: " + errorMessage.length() + "\r\n\r\n");
-					dos.writeBytes(errorMessage);
-					s.close();
-					continue;
-				} else if (!file.canRead()) {
-					String errorMessage = "You have no permission to access " + filename + " on this server.\r\n";
-					dos.writeBytes("HTTP/1.1 403 Forbidden\r\n");
-					dos.writeBytes("Content-length: " + errorMessage.length() + "\r\n\r\n");
-					dos.writeBytes(errorMessage);
-					s.close();
-					continue;
-				} else {
+					System.out.println("GET" + scriptName);
+					req_method = "GET";
+
+					File file = new File(scriptPath);
+					
+					// Check for file permission or not found error.
+					if (!file.exists() || !file.isFile()) {
+						String error = error404(scriptName);
+						dos.writeBytes(error);
+						s.close();
+						continue;
+					} else if (!file.canRead()) {
+						String error = error403(scriptName);
+						dos.writeBytes(error);
+						s.close();
+						continue;
+					}
+					
 					// Assume everything is OK then.  Send back a reply.
 					dos.writeBytes("HTTP/1.1 200 OK\r\n");
 
-					
-					if (filename.endsWith(".pl")) 
+					if (scriptName.endsWith(".pl")) 
 					{
-						System.out.println("Executing Perl script.");
-						String [] env = {"REQUEST_METHOD="+req_method, "QUERY_STRING="+query_string, "CONTENT_TYPE="+content_type, "CONTENT_LENGTH="+content_length};
-						//Process todo = Runtime.getRuntime().exec("/usr/bin/perl " + filepath, env);
-						Process todo = Runtime.getRuntime().exec(filepath, env);
-					    BufferedReader reader = new BufferedReader(new InputStreamReader(todo.getInputStream()));
+						String[] env = {"REQUEST_METHOD="+req_method, "QUERY_STRING="+query_string};
+						String todo = runPerlScript(scriptPath, env);
 						
+					    dos.writeBytes("Content-length: " + (todo.length()-47) + "\r\n");
+						dos.writeBytes(todo);
 						
-						dos.writeBytes("Content-length: " + file.length() + "\r\n");
-						
-						//dos.writeBytes("Content-type: text/pl\r\n");
 					} else {
-						
 						// We send back some HTTP response headers.
 						dos.writeBytes("Content-length: " + file.length() + "\r\n");
 	
 						// We could have use Files.probeContentType to find 
 						// the content type of the requested file, but let 
 						// me do the poor man approach here.
-						if (filename.endsWith(".html")) 
+						if (scriptName.endsWith(".html")) 
 						{
 							dos.writeBytes("Content-type: text/html\r\n");
 						}
-						if (filename.endsWith(".jpg")) 
+						if (scriptName.endsWith(".jpg")) 
 						{
 							dos.writeBytes("Content-type: image/jpeg\r\n");
 						}
@@ -193,14 +205,63 @@ class WebServer {
 							size = fis.read(buffer);
 						}
 					}
-
 					
 					dos.flush();
-
 					// Finally, close the socket and get ready for
 					// another connection.
 					s.close();
+					continue;
+					
+				} else if (tokens[0].equals("POST")) {
+					
+					scriptName = tokens[1];
+					scriptPath = WEB_ROOT + scriptName;
+					
+					System.out.println("POST " + scriptName);
+					
+					while (!line.equals("")) {
+						if (line.contains("Content-Type: ")) {
+							content_type = (String) line.substring("Content-Type: ".length());
+							
+						} else if (line.contains("Content-Length: ")) {
+							content_length = (String) line.substring("Content-Length: ".length());
+							
+						}
+						System.out.println(line);
+						line = br.readLine();
+					}
+					int ch = br.read();
+					
+					String scriptInput = "" + (char) ch;
+					while (br.ready()) {
+						ch = br.read();
+						scriptInput += (char) ch;
+					}
+
+					req_method = "POST";
+					System.out.println(content_type);
+					System.out.println(content_length);
+					
+					System.out.println(scriptInput);
+					
+					String[] env = {"REQUEST_METHOD="+req_method, "CONTENT_TYPE="+content_type, "CONTENT_LENGTH="+content_length};
+					String todo = runPerlScript(scriptPath, env);
+				    
+					System.out.println("hello");
+					dos.writeBytes("Content-length: " + todo.length() + "\r\n");
+					dos.writeBytes(todo);
+					dos.flush();
+					
+					s.close();
+					continue;
+				} else {
+					String error = error400();
+					dos.writeBytes(error);
+					s.close();
+					continue;
 				}
+
+				
 				
 			}
 			catch (IOException e)
